6cf66883fb05cbd3ec38d29b766c324865b00dd0
multiverseweb <multiverse.letter@gmail.com>
2025-03-23 23:05:55 +0530

    tabulate

--- code/main.py
+++ code/main.py
@@ -1,19 +1,34 @@
 import mysql.connector as my
+import tkinter as tk
 from tkinter import messagebox
+from tabulate import tabulate
 
 def init():
-    host = input("Enter your MySQL localhost name: ")
-    name = input("Enter your MySQL username: ")
-    password = input("Enter your MySQL password: ")
+    #root = tk.Tk()
+
+    #host = input("Enter your MySQL localhost name: ")
+    #name = input("Enter your MySQL username: ")
+    #password = input("Enter your MySQL password: ")
+    database = input("Enter database name: ")
 
     try:
-        conn = my.connect(host=host, user=name, password=password, database="numis")
+        conn = my.connect(host="localhost", user="root", password="tejas123", database=database)
         cursor = conn.cursor()
-        messagebox.showinfo("Success", "Connection Successful!")
-        cursor.close()
-        conn.close()
+        print("Success", "Connection Successful!")
+        parse(conn, cursor)
     
     except my.Error as e:
-        messagebox.showerror("Error", f"Database Connection Failed!\n{e}")
-        
+        print("Error", f"Database Connection Failed!\n{e}")
+    cursor.close()
+    conn.close()
+
+def parse(con, cursor):
+    cursor.execute("SHOW TABLES")
+    tables = cursor.fetchall()
+    for table in tables:
+        cursor.execute("DESCRIBE " + table[0])
+        columns = cursor.fetchall()
+        print(f"\n=========================Schema of {table[0]}========================")
+        print(tabulate(columns, headers=['Field', 'Type', 'Null', 'Key', 'Default', 'Extra'],tablefmt="pretty"))
+
 init()
